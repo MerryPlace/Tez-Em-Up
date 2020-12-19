bcc74a190dcaedc14295cb7f80ca2fbb9c0c673a
Noah Ortega <38738663+NoahOrtega@users.noreply.github.com>
2020-12-18 19:56:09 -0600

    Initial Commit - GUI
    First actual commit. Rough draft of GUI

---
+++ Assets/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject myUIGO;
+    UIManager myUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myUI = myUIGO.GetComponent<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ActionPressed()
+    {
+        Debug.Log("You have clicked the Action!");
+    }
+
+    public void SwitchBoardPressed(int buttonNum)
+    {
+        myUI.UpdateSwitchBoard(buttonNum);
+    }
+
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public Sprite[] portraits;
+    public GameObject portraitUI;
+    public Sprite[] highlights;
+    public GameObject switchUI;
+    public Sprite[] hearts;
+    public GameObject heartUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void UpdateSwitchBoard(int buttonNum)
+    {
+        portraitUI.GetComponent<Image>().sprite = portraits[buttonNum];
+        switchUI.GetComponent<Image>().sprite = highlights[buttonNum];
+    }
+}
