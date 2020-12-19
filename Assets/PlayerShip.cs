0926a051f693aae0b00458619099494ef9b0edd4
Noah Ortega <38738663+NoahOrtega@users.noreply.github.com>
2020-12-19 02:21:21 -0600

    Added basic ship movement

---
+++ Assets/PlayerShip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 targetPos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, transform.position.y, 0);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+
+    }
+}
